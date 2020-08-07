4ae555cfbcbff244c1b13571b1806435d81873b7
Jinyoung Ahn <yj6210ag@gmail.com>
2020-08-07 11:13:07 +0900

    Update: 닫기, x toggle

--- daumpage/toggle.js
+++ daumpage/toggle.js
@@ -2,8 +2,16 @@
 document.querySelector('.navbar-toggle').addEventListener('click', function() {
     document.querySelector('.navbar.hidden').classList.toggle('collapsed');
 });
+// 더보기 닫기 토글
+document.querySelector('.nb_exit.b').addEventListener('click', function() {
+    document.querySelector('.navbar.hidden').classList.toggle('collapsed');
+});
 
 // 날씨 토글
 document.querySelector('#temp-toggle').addEventListener('click', function() {
+    document.querySelector('.temp.hidden').classList.toggle('collapsed');
+});
+// 날씨 닫기 토글
+document.querySelector('#X').addEventListener('click', function() {
     document.querySelector('.temp.hidden').classList.toggle('collapsed');
 });
